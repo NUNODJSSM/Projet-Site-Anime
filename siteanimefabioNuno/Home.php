9a781df5934e62584662ad76b7f82747e63293f9
NUNODJSSM <nuno.djssm@eduge.ch>
2021-05-07 14:26:17 +0200

    test

---
+++ siteanimefabioNuno/Home.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel='stylesheet' type='text/css' href='css/style.css'>
+    <!--
+    Auteurs:    Nuno,Fabio
+    Date:       06.05.2021
+    Project:    faire un site anime/Manga
+-->
+<!--
+    ne pas mettre Type="text" dans style si non l'image ne va pas aparaitre
+-->
+   
+
+
+
+</head>
+    <body>
+        <header>
+            le logo doit être la
+        </header>
+
+
+        <main>
+    <nav> 
+        <ul>
+            <li>
+                <a href="Anime.php">Anime</a> 
+            </li>
+            <li>
+                  /
+            </li>
+            <li>
+                <a href="Manga.php">Manga</a>
+            </li>
+        </ul>
+    </nav>
+       
+             <?php
+            //  echo"<form  action=\"Anime.php\" method=\"POST\"><input id=\"Anime\" type=\"submit\" class=\"bouton\" value=\"Anime\" name=\"Anime\"></form>";
+            //  echo"<form  action=\"Manga.php\" method=\"POST\"><input id=\"Manga\" type=\"submit\" class=\"bouton\" value=\"Manga\" name=\"Manga\"></form>";
+             ?>
+        </main>
+    
+     <!-- <div> -->
+      <!-- - (1..20).each do |i| -->
+        <!-- %div  -->
+     <!-- </div> -->
+        <footer>
+            <p>Github : gbfdbghfbdgdfg</p>
+            <p>fhbgdfbghdfjhgbfdbgjfd</p>
+            <p>fait par : Nuno,Fabio</p>
+        </footer>
+    </body>
+</html>
